f7f5dfb6187352e43d4e026201f5471fd23bb601
Ioan-Octavian Teulea <ioan.teulea@student.unitbv.ro>
2023-10-28 15:08:11 +0300

    adding class Game

---
+++ TroubleShooters/TroubleShooters/Game.cpp
@@ -0,0 +1,8 @@
+#include "Game.h"
+
+Game::Game(Board& gameBoard, Player& p1, Player& p2) : board(gameBoard), player1(p1), player2(p2), currentPlayer(&player1) 
+{
+    // Initialize the game engine
+}
+
+
---
+++ TroubleShooters/TroubleShooters/Game.h
@@ -0,0 +1,14 @@
+#pragma once
+#include"Board.h"
+#include"Player.h"
+class Game
+{
+public:
+    Board& board; // Referin?? la tabla de joc
+    Player& player1; // Referin?? la primul juc?tor
+    Player& player2; // Referin?? la al doilea juc?tor
+    Player* currentPlayer; // Pointer c?tre juc?torul curent
+    Game(Board& gameBoard, Player& p1, Player& p2);
+
+};
+
