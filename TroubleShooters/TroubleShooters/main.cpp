ad9b8e2619459efde2e32a6e7bd4b4baeda6deb2
Ioan-Octavian Teulea <ioan.teulea@student.unitbv.ro>
2023-11-01 20:51:29 +0200

    Qt window

--- TroubleShooters/TroubleShooters/main.cpp
+++ TroubleShooters/TroubleShooters/main.cpp
@@ -1,1 +1,12 @@
-#include<iostream>
+#include <QApplication>
+#include "MyWindow.h"
+
+int main(int argc, char* argv[]) {
+    QApplication app(argc, argv);
+
+    MyWindow mainWindow;
+    mainWindow.setWindowTitle("Fereastra mea");
+    mainWindow.show();
+
+    return app.exec();
+}
