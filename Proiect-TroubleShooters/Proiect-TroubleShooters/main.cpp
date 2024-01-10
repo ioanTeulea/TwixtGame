648f18e4c87037500cb264519f21efcbfa04a60f
Gabi076 <gabriel.titescu@student.unitbv.ro>
2024-01-10 14:52:12 +0200

    Connected QT interface with game logic

--- Proiect-TroubleShooters/Proiect-TroubleShooters/main.cpp
+++ Proiect-TroubleShooters/Proiect-TroubleShooters/main.cpp
@@ -11,25 +11,34 @@
 int main(int argc, char* argv[]) {
 
     QApplication app(argc, argv);
-
     MyWindow myWindow;
 
     // Creeaz? fereastra de dialog personalizat
     PlayerInputDialog playerDialog;
 
-
-    
     Board board;
     Game Twixt(board);
-   // Twixt.Setup();
 
     QObject::connect(&playerDialog, &PlayerInputDialog::acceptedWithNames, &Twixt, &Game::setPlayerNames);
-    QObject::connect(&playerDialog, &PlayerInputDialog::acceptedWithBoardSize, &Twixt, &Game::setBoardSize); 
+    QObject::connect(&playerDialog, &PlayerInputDialog::acceptedWithBoardSize, &Twixt, &Game::setBoardSize);
+    QObject::connect(&playerDialog, &PlayerInputDialog::acceptedWithBoardSize, myWindow.getGameScene(), &GameScene::setBoardSize);
     QObject::connect(&playerDialog, &PlayerInputDialog::acceptedWithDifficulty, &Twixt, &Game::setDifficulty);
+    QObject::connect(myWindow.getGameScene(), &GameScene::circleClicked, &Twixt, &Game::action_addPawn);
+    QObject::connect(myWindow.getGameScene(), &GameScene::bridgeClicked, &Twixt, &Game::action_placeBridge);
+    QObject::connect(myWindow.getGameScene(), &GameScene::deleteBridgeClicked, &Twixt, &Game::action_deleteBridge);
+    QObject::connect(myWindow.getGameScene()->getNextPlayerButton(), &QPushButton::clicked, &Twixt, &Game::switchPlayer);
+    QObject::connect(&Twixt, &Game::currentPlayerColors, myWindow.getGameScene(), &GameScene::PlayerColors);
     playerDialog.exec();
-    
+    Twixt.Setup();
+    QObject::connect(&Twixt, &Game::gameFinished, &myWindow, [&myWindow, &Twixt]() {
+        QObject::disconnect(myWindow.getGameScene(), &GameScene::circleClicked, &Twixt, &Game::action_addPawn);
+        QObject::disconnect(myWindow.getGameScene(), &GameScene::bridgeClicked, &Twixt, &Game::action_placeBridge);
+        QObject::disconnect(myWindow.getGameScene(), &GameScene::deleteBridgeClicked, &Twixt, &Game::action_deleteBridge);
+        QObject::disconnect(myWindow.getGameScene()->getNextPlayerButton(), &QPushButton::clicked, &Twixt, &Game::switchPlayer);
+        // Deconecta?i orice alte semnale ?i sloturi între obiectele implicate
+        });
     // Începe jocul
-      Twixt.Play();
+      //Twixt.Play();
 
     return app.exec();
 }
