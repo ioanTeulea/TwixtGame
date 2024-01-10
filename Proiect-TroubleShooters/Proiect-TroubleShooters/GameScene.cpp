ffeec4c5f2c099001d4ab22c7728404c844be936
19Andi <andrei.titescu@student.unitbv.ro>
2024-01-10 14:35:23 +0200

    Worked on Qt game scene

--- Proiect-TroubleShooters/Proiect-TroubleShooters/GameScene.cpp
+++ Proiect-TroubleShooters/Proiect-TroubleShooters/GameScene.cpp
@@ -1,9 +1,143 @@
 #include "GameScene.h"
 
-GameScene::GameScene(QObject* parent):QGraphicsScene(parent), startEllipse(nullptr)
+
+GameScene::GameScene(QObject* parent, int initialWidth, int initialHeight) :QGraphicsScene(parent), startEllipse(nullptr)
 {
+
+    Width = initialWidth;
+    Height = initialHeight;
+    if (!nextPlayerButton) {
+        nextPlayerButton = new QPushButton("Next Player", nullptr);
+        nextPlayerButton->setEnabled(false);
+    }
+    drawGameBoard();
+    connect(nextPlayerButton, &QPushButton::clicked, this, &GameScene::switchColor);
+    piecePlaced = false;
+}
+
+void GameScene::showCoordinates(qreal x, qreal y)
+{
+    int X = static_cast<int>(x);
+    int Y = static_cast<int>(y);
+
+    qDebug() << "x= " << X << " y=: " << Y;
+}
+
+QPushButton* GameScene::getNextPlayerButton() const
+{
+    return nextPlayerButton;
+}
+
+void GameScene::setBoardSize(int newSize)
+{
+    boardSize = newSize;
+    drawGameBoard();
+}
+
+void GameScene::mousePressEvent(QGraphicsSceneMouseEvent* event)
+{
+
+
+    QGraphicsItem* item = itemAt(event->scenePos(), QTransform());
+    if (item && item->type() == QGraphicsEllipseItem::Type)
+    {
+        startEllipse = qgraphicsitem_cast<QGraphicsEllipseItem*>(item);
+        QColor color = startEllipse->data(2).value<QColor>();
+        bool isOk = false;
+        if (!piecePlaced)
+        {
+            emit circleClicked(color, startEllipse->data(1).toUInt(), startEllipse->data(0).toUInt(), isOk);
+            if (isOk)
+            {
+                startEllipse->setBrush(currentColor);  // Face cercul rosu
+                startEllipse->setData(2, currentColor);
+                nextPlayerButton->setEnabled(true);
+                nextPlayerButton->setStyleSheet("QPushButton {"
+                    "   background-color: lightblue;"  // culoare de fundal
+                    "   color: black;"                  // culoare text
+                    "   border: 2px solid black;"       // grosimea bordurii ?i culoare neagr?
+                    "   border-radius: 10px;"           // col?uri rotunde
+                    "   padding: 5px 10px;"             // spa?iere ?ntre text ?i marginile butonului
+                    "   font-family: 'Times New Roman';" // fontul textului
+                    "   font-size: 12px;"               // dimensiunea fontului
+                    "}");
+                piecePlaced = true;
+                qreal centerX = event->scenePos().x();
+                qreal centerY = event->scenePos().y();
+
+                uint16_t i = startEllipse->data(1).toUInt();  // Ia valoarea asociat? cu i
+                uint16_t j = startEllipse->data(0).toUInt();  // Ia valoarea asociat? cu j
+
+
+                showCoordinates(centerX, centerY);
+                qDebug() << "Cerc selectat: i = " << i << ", j = " << j;
+            }
+        }
+    }
+    else if (item && item->type() == QGraphicsLineItem::Type) {
+        QGraphicsLineItem* line = qgraphicsitem_cast<QGraphicsLineItem*>(item);
+        emit deleteBridgeClicked(line->data(0).toUInt(), line->data(1).toUInt(), line->data(2).toUInt(), line->data(3).toUInt());
+        removeItem(item);
+    }
+
+    QGraphicsScene::mousePressEvent(event);
+}
+
+
+void GameScene::mouseReleaseEvent(QGraphicsSceneMouseEvent* event)
+{
+    if (startEllipse)
+    {
+        QGraphicsItem* item = itemAt(event->scenePos(), QTransform());
+        if (item && item->type() == QGraphicsEllipseItem::Type)
+        {
+            QGraphicsEllipseItem* endEllipse = qgraphicsitem_cast<QGraphicsEllipseItem*>(item);
+            //trebuie verificata culoarea lui endElipse
+            if (endEllipse != startEllipse)
+            {
+                bool isOk = false;
+                emit bridgeClicked(startEllipse->data(1).toUInt(), startEllipse->data(0).toUInt(), endEllipse->data(1).toUInt(), endEllipse->data(0).toUInt(), isOk);
+                if (isOk)
+                {
+                    QGraphicsLineItem* line = new QGraphicsLineItem();
+                    QPen pen(currentColor, 2, Qt::SolidLine);  // Linie ro?ie(fa tu ce culoare e), grosime 2, linie punctat?
+                    line->setPen(pen);
+                    line->setLine(startEllipse->rect().center().x(), startEllipse->rect().center().y(),
+                        endEllipse->rect().center().x(), endEllipse->rect().center().y());
+                    line->setData(0, startEllipse->data(1).toUInt());
+                    line->setData(1, startEllipse->data(0).toUInt());
+                    line->setData(2, endEllipse->data(1).toUInt());
+                    line->setData(3, endEllipse->data(0).toUInt());
+                    addItem(line);
+                    lines.append(line);
+
+                }
+            }
+        }
+
+
+        startEllipse = nullptr;
+    }
+
+    QGraphicsScene::mouseReleaseEvent(event);
+}
+
+void GameScene::drawGameBoard()
+{
+    qreal buttonWidth = Width * 0.15;     // 15% din l??imea scenei
+    qreal buttonHeight = Height * 0.07;; // 5% din ?n?l?imea scenei
+    qreal buttonX = -Width / 2.0 + 10;;  // Ajusteaz? pozi?ia pe axa X
+    qreal buttonY = -Height / 2.0 + 10;  // Ajusteaz? pozi?ia pe axa Y
+
+
+    nextPlayerButton->setGeometry(buttonX, buttonY, buttonWidth, buttonHeight);
+    nextPlayerButton->setEnabled(false);
+
+    // Conecteaz? semnalul butonului la o func?ie de tratare
+
+
     qreal radius = cellSize / 4.0; // raza cercului
-    qreal distance = 1.0 * cellSize; // distan?a între cercuri
+    qreal distance = 1.0 * cellSize; // distan?a ?ntre cercuri
 
     // Ini?ializarea dimensiunilor ?i coordonatelor scenei
     qreal sceneWidth = boardSize * distance;
@@ -20,6 +154,9 @@ GameScene::GameScene(QObject* parent):QGraphicsScene(parent), startEllipse(nullp
             circle->setPen(QPen(Qt::black));
             circle->setBrush(QBrush(Qt::gray));
             circle->setFlag(QGraphicsItem::ItemIsSelectable);
+            circle->setData(0, i);  // ?n prima valoare (index 0), stocheaz? i
+            circle->setData(1, j);  // ?n a doua valoare (index 1), stocheaz? j
+            circle->setData(2, QBrush(Qt::gray)); //In a treia valoarea stocheaza culoarea
             addItem(circle);
         }
     }
@@ -39,77 +176,24 @@ GameScene::GameScene(QObject* parent):QGraphicsScene(parent), startEllipse(nullp
     addLine(middle, yOffset, middle, yOffset + sceneHeight, blackPen);
 
     lines = QList<QGraphicsLineItem*>();
+
+    // Adaug? butonul ?n scen?
+    QGraphicsProxyWidget* proxyButton = new QGraphicsProxyWidget();
+    proxyButton->setWidget(nextPlayerButton);
+    addItem(proxyButton);
 }
-
-void GameScene::showCoordinates(qreal x, qreal y)
+void GameScene::switchColor()
 {
-    int X = static_cast<int>(x);
-    int Y = static_cast<int>(y);
-
-    qDebug() << "x= " << X << " y=: " << Y;
+    if (currentColor == player1Color)
+        currentColor = player2Color;
+    else
+        currentColor = player1Color;
+    nextPlayerButton->setEnabled(false);
+    piecePlaced = false;
 }
-
-void GameScene::mousePressEvent(QGraphicsSceneMouseEvent* event)
+void GameScene::PlayerColors(QColor color1, QColor color2)
 {
-    
-        QGraphicsItem* item = itemAt(event->scenePos(), QTransform());
-        if (item && item->type() == QGraphicsEllipseItem::Type)
-        {
-            startEllipse = qgraphicsitem_cast<QGraphicsEllipseItem*>(item);
-            startEllipse->setBrush(QBrush(Qt::red));  // Face cercul rosu
-
-            qreal centerX = event->scenePos().x();
-            qreal centerY = event->scenePos().y();
-
-            showCoordinates(centerX, centerY);
-        }
-   
-
-    QGraphicsScene::mousePressEvent(event);
-}
-
-//void GameScene::mouseMoveEvent(QGraphicsSceneMouseEvent* event)
-//{
-//    if (selectedEllipse)
-//    {
-//        QPointF start = selectedEllipse->rect().center();
-//        QPointF end = event->scenePos();
-//
-//        if (!draggingLine->scene())
-//        {
-//            addItem(draggingLine);
-//        }
-//
-//        draggingLine->setLine(start.x(), start.y(), end.x(), end.y());
-//    }
-//
-//    QGraphicsScene::mouseMoveEvent(event);
-//}
-
-void GameScene::mouseReleaseEvent(QGraphicsSceneMouseEvent* event)
-{
-    if (startEllipse)
-    {
-        QGraphicsItem* item = itemAt(event->scenePos(), QTransform());
-        if (item && item->type() == QGraphicsEllipseItem::Type)
-        {
-            QGraphicsEllipseItem* endEllipse = qgraphicsitem_cast<QGraphicsEllipseItem*>(item);
-            if (endEllipse != startEllipse)
-            {
-                endEllipse->setBrush(QBrush(Qt::red));  // Face cercul rosu
-                QGraphicsLineItem* line = new QGraphicsLineItem();
-                QPen pen(Qt::red, 2, Qt::SolidLine);  // Linie ro?ie, grosime 2, linie punctat?
-                line->setPen(pen);
-                line->setLine(startEllipse->rect().center().x(), startEllipse->rect().center().y(),
-                    endEllipse->rect().center().x(), endEllipse->rect().center().y());
-                addItem(line);
-                lines.append(line);
-            }
-        }
-
-       
-        startEllipse = nullptr;
-    }
-
-    QGraphicsScene::mouseReleaseEvent(event);
+    player1Color = color1;
+    player2Color = color2;
+    currentColor = player1Color;
 }
--- Proiect-TroubleShooters/Proiect-TroubleShooters/GameScene.h
+++ Proiect-TroubleShooters/Proiect-TroubleShooters/GameScene.h
@@ -12,26 +12,44 @@
 #include <QGraphicsRectItem>
 #include <QGraphicsSceneMouseEvent>
 #include <QDebug>
+#include <QPushButton>
+#include <QGraphicsProxyWidget>
+
 
 class GameScene : public QGraphicsScene
 {
     Q_OBJECT
 
 public:
-    GameScene(QObject* parent = nullptr);
+    GameScene(QObject* parent = nullptr, int initialWidth = 0, int initialHeight = 0);
     void showCoordinates(qreal x, qreal y);
+    QPushButton* getNextPlayerButton() const;
 
 protected:
     void mousePressEvent(QGraphicsSceneMouseEvent* event) override;
-   // void mouseMoveEvent(QGraphicsSceneMouseEvent* event) override;
     void mouseReleaseEvent(QGraphicsSceneMouseEvent* event) override;
+    void drawGameBoard();
+    void switchColor();
+    int Width;
+    int Height;
 
 private:
     qreal cellSize = 50;
-    qint32 boardSize = 10;
+    int boardSize;
     QGraphicsEllipseItem* startEllipse;
     QList<QGraphicsLineItem*> lines;
+    QPushButton* nextPlayerButton = nullptr;
+    QColor player1Color;
+    QColor player2Color;
+    QColor currentColor;
+    bool piecePlaced;
+signals:
+    void circleClicked(QColor color, const uint16_t i, const uint16_t j, bool& isOk);
+    void bridgeClicked(const uint16_t x1, const uint16_t y1, const uint16_t x2, const uint16_t y2, bool& isOk);
+    void deleteBridgeClicked(const uint16_t x1, const uint16_t y1, const uint16_t x2, const uint16_t y2);
+public slots:
+    void setBoardSize(int newSize);
+    void PlayerColors(QColor color1, QColor color2);
 };
 
 #endif // GAMESCENE_H
-
